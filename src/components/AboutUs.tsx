82351f3fb33e4bde398a9e03245c69196d7e802e
unknown <jul.1012@hotmail.com>
2023-10-23 22:41:20 -0500

    initialize footer

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -23,16 +23,16 @@ export const AboutUs : React.FC<AboutUsProps> = ({image,color,title,content}) =>
 
 
     return <div className="relative mt-40 grid grid-cols-2">
-    <div className="bg-cover bg-center bg-no-repeat w-screen h-screen" style={{ backgroundImage: url }}></div>
+    <div className="bg-cover bg-no-repeat w-screen h-screen" style={{ backgroundImage: url }}></div>
     <div className={colorDiv}> 
       <h1 className="text-5xl font-roboto text-white mb-4">{title}</h1> 
-      {content.map((paragraph,index) =>
-      <>
-      <p key={index} className=" text-2xl font-montserrat text-base text-white">{paragraph}</p>
-      <br></br>
-      </>
-      )}
+      {content.map((paragraph, index) => (
+        <p key={index} className="text-2xl font-montserrat text-base text-white">
+          {paragraph}
+        </p>
+      ))}
     </div>
+    <br></br>
   </div>;
 
 
--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -2,31 +2,52 @@ import React from 'react';
 
 interface CharactersProps {
   color: string;
-  position: string;
+  initialPosition: string; 
   image: string;
   info: string;
 }
 
-export const Characters: React.FC<CharactersProps> = ({ color, position, image, info }) => {
+export const Characters: React.FC<CharactersProps> = ({ color, initialPosition, image, info }) => {
   const colorDiv: string = color;
   const urlImage: string = "images/" + image;
 
-  if (position === "start") {
+  const commonDivStyles = {
+    width: "60%",
+    height: "70%",
+    backgroundColor: colorDiv,
+    display: "flex",
+    alignItems: "center",
+    padding: "10px",
+    borderRadius: "70px",
+  };
+
+  const textStyle = {
+    fontSize: "27px",
+    color: "white",
+  };
+
+  const imageStyle = {
+    marginLeft: "-50px",
+    width: "200px",
+  };
+
+  const imageStyleRight = {
+    marginRight: "-50px",
+    width: "200px",
+  };
+
+  if (initialPosition === "start") {
     return (
-      <div className="flex justify-start ml-8">
-        <img src={urlImage} alt="Imagen" />
-        <div className="customGreen" style={{ width: "50%", height: "25%" }}>
-          {info}
-        </div>
+      <div  style={commonDivStyles} className='mt-20 justify-start ml-40' >
+        <img src={urlImage} style={imageStyle} />
+        <div style={textStyle}>{info}</div>
       </div>
     );
   } else {
     return (
-      <div className="flex justify-end mr-8">
-        <img src={urlImage} alt="Imagen" />
-        <div className={colorDiv} style={{ width: "50%", height: "25%" }}>
-          {info}
-        </div>
+      <div  style={commonDivStyles} className=" mt-20  justify-end ml-60">
+        <div style={textStyle}>{info}</div>
+        <img src={urlImage} style={imageStyleRight} />
       </div>
     );
   }
